9d6466d960c0dbc1db58463963f1dc9c5352392e
Martin de Laat <martin.delaat@nedap.com>
2024-12-04 14:13:43 +0100

    day3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,25 +1,62 @@
-import kotlin.math.absoluteValue
-
 fun main() {
     val day = 3
+    var doMode = true
+
+    fun toPair(input: String): Pair<Int, Int> {
+        val ints = input.removePrefix("mul(").removeSuffix(")").split(',').map{it.toInt()}
+        return Pair(ints.first(), ints[1])
+    }
+
+    fun scanMuls(input: String): List<Pair<Int,Int>> {
+        val regex = Regex("""mul\(\d+,\d+\)""")
+        return regex.findAll(input).toList().map{it.value}.map(::toPair)
+    }
+
+    fun scanMuls2(input: String): List<Pair<Int,Int>> {
+        if(input.startsWith("don't()")) {
+            doMode = false
+            return scanMuls2(input.substringAfter("don't()"))
+        } else if(input.startsWith("do()")) {
+            doMode = true
+            return scanMuls2(input.substringAfter("do()"))
+        }
+
+        val regex = Regex("""^mul\(\d+,\d+\)""")
+        if(regex.containsMatchIn(input)) {
+            val restString = regex.replace(input, "")
+            if (doMode) {
+                val pair = toPair(regex.find(input)!!.value)
+                return listOf(pair) + scanMuls2(restString)
+            } else {
+                return scanMuls2(restString)
+            }
+        }
+
+        if(input.count() == 0) {
+            return emptyList()
+        }
+        return scanMuls2(input.removeRange((0..0)))
+    }
+
 
     fun part1(input: List<String>): Int {
-        val test = "bla".println()
-
-
-        return 1
+        return input.sumOf {
+            scanMuls(it).sumOf { it.first * it.second }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        return input.sumOf {
+            scanMuls2(it).sumOf { it.first * it.second }
+        }
     }
 
 
     val testInput = readInput("Day0${day}_test")
-//    part1(testInput).println()
-//    part2(testInput).println()
+    part1(testInput).println()
+    part2(testInput).println()
 
     val input = readInput("Day0${day}")
-//    part1(input).println()
-//    part2(input).println()
+    part1(input).println()
+    part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,10 @@ import kotlin.io.path.readText
  */
 fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
 
+fun <T> T.p(prefix: String = ""): T {
+    println("${if (prefix.isEmpty()) "" else "$prefix: "}$this")
+    return this
+}
 /**
  * Converts string to md5 hash.
  */
@@ -15,9 +19,7 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .toString(16)
     .padStart(32, '0')
 
-/**
- * The cleaner shorthand for printing output.
- */
+
 fun Any?.println() = println(this)
 
-fun toIntList(input: String) = input.split(' ').map { it.toInt() }
+fun toIntList(input: String): List<Int> = input.split(' ').map { it.toInt() }
