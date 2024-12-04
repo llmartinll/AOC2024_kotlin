a2d8f1c1f29a9ba25eab97344a49f80154cd1917
Martin de Laat <martin.delaat@nedap.com>
2024-12-04 10:18:22 +0100

    setup

---
+++ src/Day03.kt
@@ -0,0 +1,25 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    val day = 3
+
+    fun part1(input: List<String>): Int {
+        val test = "bla".println()
+
+
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+
+    val testInput = readInput("Day0${day}_test")
+//    part1(testInput).println()
+//    part2(testInput).println()
+
+    val input = readInput("Day0${day}")
+//    part1(input).println()
+//    part2(input).println()
+}
---
+++ src/Day07.kt
@@ -0,0 +1,24 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    val day = 7
+
+    fun part1(input: List<String>): Int {
+        
+
+        return 1
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+
+    val testInput = readInput("Day0${day}_test")
+//    part1(testInput).println()
+//    part2(testInput).println()
+
+    val input = readInput("Day0${day}")
+//    part1(input).println()
+//    part2(input).println()
+}
